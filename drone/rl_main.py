932759e93f0a78ab7eeae1d2ff7ef34bcbedbf03
gilgurarieh <gil.gurarie@gmail.com>
2024-05-23 20:40:41 +0300

    updates the code

--- drone/rl_main.py
+++ drone/rl_main.py
@@ -7,7 +7,7 @@ from DroneEnv import DroneEnv  # Import your environment from DroneEnv.py
 # Create a function to plot the rewards
 def plot_rewards(rewards, title="Training Rewards"):
     plt.plot(rewards)
-    plt.xlabel('Episodes')
+    plt.xlabel('Evaluations')
     plt.ylabel('Mean Reward')
     plt.title(title)
     plt.show()
@@ -27,37 +27,40 @@ eval_callback = EvalCallback(env, best_model_save_path='./logs/',
                              deterministic=True, render=False)
 
 # Train the agent and plot rewards
-total_timesteps = 10000
+total_timesteps = 1000
 agent.learn(total_timesteps=total_timesteps, callback=eval_callback)
-mean_rewards, _ = zip(*eval_callback.evaluations_results)
-plot_rewards(mean_rewards, title="Stabilize Phase Rewards")
-
 # Save the trained agent
 agent.save("a2c_stabilize")
+# Extract evaluation results and plot rewards
+mean_rewards = [result[0] for result in eval_callback.evaluations_results]
+plot_rewards(mean_rewards, title="Stabilize Phase Rewards")
+
+
 
 # STAGE 2 - vertical
+total_timesteps = 5000
 env.reward_type = "vertical"
 agent = A2C.load("a2c_stabilize", env=env)
 agent.learn(total_timesteps=total_timesteps, callback=eval_callback)
-mean_rewards, _ = zip(*eval_callback.evaluations_results)
-plot_rewards(mean_rewards, title="Vertical Phase Rewards")
 agent.save("a2c_vertical")
+# Extract evaluation results and plot rewards
+mean_rewards = [result[0] for result in eval_callback.evaluations_results]
+plot_rewards(mean_rewards, title="Vertical Phase Rewards")
+
 
 # STAGE 3 - position
 env.reward_type = "position"
 agent = A2C.load("a2c_vertical", env=env)
 agent.learn(total_timesteps=total_timesteps, callback=eval_callback)
-mean_rewards, _ = zip(*eval_callback.evaluations_results)
-plot_rewards(mean_rewards, title="Position Phase Rewards")
 agent.save("a2c_position")
+# Extract evaluation results and plot rewards
+mean_rewards = [result[0] for result in eval_callback.evaluations_results]
+plot_rewards(mean_rewards, title="Position Phase Rewards")
+
 
 # Test the model
 obs = env.reset()
-reward_list = []
 for _ in range(1000):
     action, _states = agent.predict(obs, deterministic=True)
     obs, rewards, dones, info = env.step(action)
-    reward_list.append(rewards)
     env.render()
-
-print(f"MEAN REWARD IN TEST: {sum(reward_list)/len(reward_list)}")
